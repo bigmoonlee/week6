641f31d8f1c9b9f436ec7c0dbeb3daca1b4940bd
bigmoonlee <107873547+bigmoonlee@users.noreply.github.com>
2022-09-14 23:29:14 +0800

    week6 upd

--- packages/hardhat/hardhat.config.js
+++ packages/hardhat/hardhat.config.js
@@ -23,7 +23,7 @@ const { isAddress, getAddress, formatUnits, parseUnits } = utils;
 */
 
 //
-// Select the network you want to deploy to here:
+// Select the network you want to deploy to here: s
 //
 const defaultNetwork = "localhost";
 
